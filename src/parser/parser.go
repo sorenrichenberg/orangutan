a459e5da1111a9c480e93ece3bbcae102ea67945
Soren Richenberg <sorensrichenberg@gmail.com>
2024-06-11 22:29:33 -0700

    feature(parser): implemented parsing function maps for pratt parsing

--- src/parser/parser.go
+++ src/parser/parser.go
@@ -9,12 +9,20 @@ import (
 	"orangutan/token"
 )
 
+type (
+	prefixParseFn func() ast.Expression
+	infixParseFn  func(ast.Expression) ast.Expression
+)
+
 type Parser struct {
 	l      *lexer.Lexer
 	errors []string
 
 	curToken  token.Token
 	peekToken token.Token
+
+	prefixParseFns map[token.TokenType]prefixParseFn
+	infixParseFns  map[token.TokenType]infixParseFn
 }
 
 func New(l *lexer.Lexer) *Parser {
@@ -118,4 +126,13 @@ func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
 	}
 
 	return stmt
+}
+
+// func (p *Parser) parseExpressionStatement() *ast.ExpressionStatement {}
+
+func (p *Parser) registerPrefix(tokenType token.TokenType, fn prefixParseFn) {
+	p.prefixParseFns[tokenType] = fn
+}
+func (p *Parser) registerInfix(tokenType token.TokenType, fn infixParseFn) {
+	p.infixParseFns[tokenType] = fn
 }
