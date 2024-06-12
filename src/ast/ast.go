1967bf8898f14a4277de156c433bffb28c0e53d2
Soren Richenberg <sorensrichenberg@gmail.com>
2024-06-11 22:27:34 -0700

    feature(ast): added string methods for ast nodes and ast tests for them

--- src/ast/ast.go
+++ src/ast/ast.go
@@ -3,11 +3,13 @@
 package ast
 
 import (
+	"bytes"
 	"orangutan/token"
 )
 
 type Node interface {
 	TokenLiteral() string
+	String() string
 }
 
 type Statement interface {
@@ -32,6 +34,16 @@ func (p *Program) TokenLiteral() string {
 	}
 }
 
+func (p *Program) String() string {
+	var out bytes.Buffer
+
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+
+	return out.String()
+}
+
 type Identifier struct {
 	Token token.Token // always IDENT token
 	Value string
@@ -39,6 +51,7 @@ type Identifier struct {
 
 func (i *Identifier) expressionNode()      {}
 func (i *Identifier) TokenLiteral() string { return i.Value }
+func (i *Identifier) String() string       { return i.Value }
 
 type LetStatement struct {
 	Token token.Token // always LET token
@@ -48,6 +61,21 @@ type LetStatement struct {
 
 func (ls *LetStatement) statementNode()       {}
 func (ls *LetStatement) TokenLiteral() string { return ls.Token.Lexeme }
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(ls.TokenLiteral() + " ")
+	out.WriteString(ls.Name.String() + " ")
+	out.WriteString("= ")
+
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
 
 type ReturnStatement struct {
 	Token       token.Token // always RETURN token
@@ -56,3 +84,35 @@ type ReturnStatement struct {
 
 func (rs *ReturnStatement) statementNode()       {}
 func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Lexeme }
+func (rs *ReturnStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(rs.TokenLiteral() + " ")
+
+	if rs.ReturnValue != nil {
+		out.WriteString(rs.ReturnValue.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
+
+type ExpressionStatement struct {
+	Token           token.Token
+	ExpressionValue Expression
+}
+
+func (es *ExpressionStatement) statementNode()       {}
+func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Lexeme }
+func (es *ExpressionStatement) String() string {
+	var out bytes.Buffer
+
+	if es.ExpressionValue != nil {
+		out.WriteString(es.ExpressionValue.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
--- src/ast/ast_test.go
+++ src/ast/ast_test.go
@@ -1,3 +1,30 @@
 // ast/ast_test.go
 
 package ast
+
+import (
+	"orangutan/token"
+	"testing"
+)
+
+func TestString(t *testing.T) {
+	program := &Program{
+		Statements: []Statement{
+			&LetStatement{
+				Token: token.Token{Type: token.LET, Lexeme: "let"},
+				Name: &Identifier{
+					Token: token.Token{Type: token.IDENT, Lexeme: "myVar"},
+					Value: "myVar",
+				},
+				Value: &Identifier{
+					Token: token.Token{Type: token.IDENT, Lexeme: "anotherVar"},
+					Value: "anotherVar",
+				},
+			},
+		},
+	}
+
+	if program.String() != "let myVar = anotherVar;" {
+		t.Errorf("program.String() wrong. got=%q", program.String())
+	}
+}
