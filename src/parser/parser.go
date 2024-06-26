522f6ff47f1fcd9733b663137394ee781a54461a
Soren Richenberg <sorensrichenberg@gmail.com>
2024-06-25 20:17:19 -0700

    fix(parser): fixed bug parseBlockStatement() that was causing parser to get stuck on LBRACE

--- src/parser/parser.go
+++ src/parser/parser.go
@@ -323,6 +323,8 @@ func (p *Parser) parseBlockStatement() *ast.BlockStatement {
 	block := &ast.BlockStatement{Token: p.curToken}
 	block.Statements = []ast.Statement{}
 
+	p.nextToken()
+
 	for !p.curTokenIs(token.RBRACE) && !p.curTokenIs(token.EOF) {
 		stmt := p.parseStatement()
 		if stmt != nil {
