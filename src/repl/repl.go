8996ad920b7b9471c8f76a53634c0ba7a4c5f2f0
Soren Richenberg <sorensrichenberg@gmail.com>
2024-06-27 22:51:16 -0700

    feature(repl): updated the repl from lexer output to parser output

--- src/repl/repl.go
+++ src/repl/repl.go
@@ -7,7 +7,7 @@ import (
 	"fmt"
 	"io"
 	"orangutan/lexer"
-	"orangutan/token"
+	"orangutan/parser"
 )
 
 const PROMPT = "🦧🔸"
@@ -16,7 +16,7 @@ func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
 
 	for {
-		fmt.Fprintf(out, PROMPT)
+		fmt.Fprint(out, PROMPT)
 		scanned := scanner.Scan()
 		if !scanned {
 			return
@@ -24,9 +24,21 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Fprintf(out, "%+v\n", tok)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
