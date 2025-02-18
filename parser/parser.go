93b1b4dded77bbc70af27a13ffd78cba20ba8591
Soren Richenberg <sorensrichenberg@gmail.com>
2025-02-17 22:08:55 -0800

    chores(parser): removed an if statement wrapper that is always true

--- parser/parser.go
+++ parser/parser.go
@@ -139,9 +139,8 @@ func (p *Parser) ParseProgram() *ast.Program {
 
 	for p.curToken.Type != token.EOF {
 		stmt := p.parseStatement()
-		if stmt != nil {
-			program.Statements = append(program.Statements, stmt)
-		}
+		program.Statements = append(program.Statements, stmt)
+
 		p.nextToken()
 	}
 
@@ -349,9 +348,8 @@ func (p *Parser) parseBlockStatement() *ast.BlockStatement {
 
 	for !p.curTokenIs(token.RBRACE) && !p.curTokenIs(token.EOF) {
 		stmt := p.parseStatement()
-		if stmt != nil {
-			block.Statements = append(block.Statements, stmt)
-		}
+		block.Statements = append(block.Statements, stmt)
+
 		p.nextToken()
 	}
 
